2b368466fd0ce5771ed18c314278dbbebde4bf35
deorth-kku <deorth@outlook.com>
2026-06-09 20:04:52 +0800

    no STW

--- common/geodata/ip_matcher.go
+++ common/geodata/ip_matcher.go
@@ -3,7 +3,6 @@ package geodata
 import (
 	"context"
 	"net/netip"
-	"runtime"
 	"slices"
 	"sort"
 	"strings"
@@ -908,10 +907,6 @@ func (f *IPSetFactory) createFrom(yield func(func(*CIDR)) error) (*IPSet, error)
 	if err != nil {
 		return nil, err
 	}
-
-	// peak mem
-	runtime.GC()
-	defer runtime.GC()
 
 	ipv4, err := ipv4Builder.IPSet()
 	if err != nil {
